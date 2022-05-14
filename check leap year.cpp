d74413ccdcf2b39b9f57ffb3892b88c771bd6be9
Sanjeeban Mukhopadhyay <94889532+sanjeeban5644@users.noreply.github.com>
2022-05-14 09:34:55 +0530

    Update check leap year.cpp

--- check leap year.cpp
+++ check leap year.cpp
@@ -2,14 +2,13 @@
 #include <stdio.h>
 int main () {
 int n ;
-printf("Enter a year \n");
+printf("\nEnter a year : ");
 scanf("%d",&n);
-if(n%4==0){
-	printf("%d is a leap year",n);
-}
-else {
-printf("%d is not a leap year",n);
-	
-}
-return 0 ;
+  if(n%4==0 && n%400==0 || n%100!=0){
+	printf("\n%d is a leap year",n);
+     }
+  else {
+        printf("\n%d is not a leap year",n);
+     }
+  return 0 ;
 }
